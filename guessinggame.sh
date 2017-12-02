5451e7c0907aa9efd6c8bb7f13b50d7241f0c261
jasakar <jasakar@gmail.com>
2017-12-02 18:25:17 +0200

    minor correction in  guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -8,9 +8,9 @@ function give_number {
 }
 # count is number of fles in this dictory
 count=$(ls -A | wc -l)
-echo "Number of files: $count"
+# echo "Number of files: $count"
 
-
+# call function 
 give_number
 
 # continue until $n eauqals $count
@@ -23,9 +23,11 @@ do
 		echo "and it is too large"
 	fi
 	echo
+	# call function 
 	give_number
 done	
 echo
 echo "Well done my friend!"
 echo "Number of files in this directory is indeed: $count"
+echo
 
